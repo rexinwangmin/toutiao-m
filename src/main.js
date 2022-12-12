d1fe2cfd30c7f0afec3d87d8c734e3d550dcd32f
dubaojun <1684825557@qq.com>
2022-12-12 16:19:51 +0800

    项目初始化-引入字体图标和vant组件库

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 // 加载全局样式
 import '@/styles/index.less'
 
+// 加载vant和vant的全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
